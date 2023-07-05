33cea9e37995982f37a895f7ca62aba22eb5d7f7
Kcpawa <95114024+Kcpawa@users.noreply.github.com>
2023-07-05 12:38:30 +0530

    Add files via upload

---
+++ math_fun.rb
@@ -0,0 +1,24 @@
+#Let's try math function today
+puts Math.sin(10)
+# >>-0.5440211108893698
+puts Math.cos(20)
+# >> 0.40808206181339196
+puts Math.log(1)
+# >> 0.0
+puts Math.log(100,1000)
+# >> 0.6666666666666667
+puts Math.sqrt(3)
+# >> 1.7320508075688772
+puts Math.tan(8)
+#>>-6.799711455220379
+puts Math::PI
+#>>3.141592653589793
+#Value of Natural Logrithum
+puts Math::E
+#>>2.718281828459045
+#error function
+puts Math::erf(1)
+#>>0.8427007929497149
+#complementary error function
+puts Math::erfc(2)
+#>> 0.004677734981047265
