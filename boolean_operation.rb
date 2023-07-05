75c0ab1a672d7c753c38e62506186441dbd60a51
Kcpawa <95114024+Kcpawa@users.noreply.github.com>
2023-07-05 13:21:59 +0530

    Add files via upload

---
+++ boolean_operation.rb
@@ -0,0 +1,16 @@
+# here we go today we going to learn boolean operation in ruby 
+#As same as all in coding language.
+#Not so tough 
+a=true 
+b=false 
+puts a
+
+a=10 
+b=20 
+c=30 
+d= a>=b   
+e= c<=a 
+f= a==10
+puts d
+puts e 
+puts f
