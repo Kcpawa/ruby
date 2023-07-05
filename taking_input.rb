b4641862f810c7202715df8ab9ebc07a4c7bc145
Kcpawa <95114024+Kcpawa@users.noreply.github.com>
2023-07-05 11:45:16 +0530

    Add files via upload

---
+++ taking_input.rb
@@ -0,0 +1,15 @@
+# here taking as string input
+puts "Enter the number:"
+a=gets.chomp()
+# print "Enter the number:"," ",a
+puts"output is #{a}"
+
+puts "Enter the integer"
+a=gets.chomp.to_i
+puts "here is the integer #{a}"
+
+#Taking a integer as input here
+a=gets.chomp.to_i
+b=gets.chomp.to_i
+c=a+b 
+print"output of #{a} and #{b} is:",c
