01b0ab4a6fa035cb1790bf2bee362dd5116769b5
Kcpawa <95114024+Kcpawa@users.noreply.github.com>
2023-07-10 14:29:10 +0530

    Add files via upload

---
+++ methods.rb
@@ -0,0 +1,27 @@
+#here we go 
+#functions 
+#functions are used as a reusability of code
+#it has a block of code.
+#and performs a specific task
+#as simple as python, what did in python to define a function as same in ruby also.
+
+#normal function declaration 
+def func()
+
+    puts"hi how are you?"
+end
+
+func()
+
+#output 
+# hi how are you?
+
+#function passing an a arguments 
+
+def func(my_data,age)
+    puts "my name is #{my_data} and my age is #{age}"
+end 
+func("pawan",21)
+
+# output
+# my name is pawan and my age is 21
